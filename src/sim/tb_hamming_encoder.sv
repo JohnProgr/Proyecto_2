92b10c74e9ae98ba17d9b4a936196efe4ecd735a
FabianChaverriV <fabianandreschaverri@gmail.com>
2026-03-31 14:20:48 -0600

    hamming

--- src/sim/tb_hamming_encoder.sv
+++ src/sim/tb_hamming_encoder.sv
@@ -2,25 +2,50 @@
 
 module tb_hamming_encoder;
 
-    logic [7:0] data_in;
-    logic [12:0] code_out;
+    reg [3:0] data_in;
+    wire [7:0] code_out;
 
-    hamming_encoder_8bit uut (
+    hamming_encoder uut (
         .data_in(data_in),
         .code_out(code_out)
     );
 
     initial begin
-        $dumpfile("wave.vcd");
-        $dumpvars(0, tb_hamming_encoder);
+        data_in = 4'b0000;
+        #10;
 
-        data_in = 8'b00000000; #10;
-        data_in = 8'b10101010; #10;
-        data_in = 8'b11110000; #10;
-        data_in = 8'b11111111; #10;
-        data_in = 8'b01010101; #10;
+        data_in = 4'b0001;
+        #10;
+
+        data_in = 4'b0010;
+        #10;
+
+        data_in = 4'b0011;
+        #10;
+
+        data_in = 4'b0100;
+        #10;
+
+        data_in = 4'b0101;
+        #10;
+
+        data_in = 4'b0111;
+        #10;
+
+        data_in = 4'b1001;
+        #10;
+
+        data_in = 4'b1010;
+        #10;
+
+        data_in = 4'b1111;
+        #10;
 
         $finish;
+    end
+
+    initial begin
+        $monitor("time=%0t | data_in=%b | code_out=%b", $time, data_in, code_out);
     end
 
 endmodule
